630f1b2df42e6fad6ca6ba0e02f1ee8ef01ce67b
devbyrieza <devbyrieza@gmail.com>
2026-07-07 13:28:10 +0700

    fix: change misleading placeholders for tinggi_badan and berat_badan

--- src/app/dashboard/pendaftar/components/tabs/DataLengkapForm.tsx
+++ src/app/dashboard/pendaftar/components/tabs/DataLengkapForm.tsx
@@ -1114,7 +1114,7 @@ export default function DataLengkapForm({
                       updateSantri("tinggi_badan", parseInt(v) || 0)
                     }
                     type="number"
-                    placeholder="150"
+                    placeholder="Contoh: 150"
                     required
                   />
                   <InputField
@@ -1125,7 +1125,7 @@ export default function DataLengkapForm({
                       updateSantri("berat_badan", parseFloat(v) || 0)
                     }
                     type="number"
-                    placeholder="45"
+                    placeholder="Contoh: 45"
                     required
                   />
                   <div className="md:col-span-2 lg:col-span-1">
