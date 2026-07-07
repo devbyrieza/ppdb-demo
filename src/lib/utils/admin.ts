2ba16c205ed25c5047da10fb25070d4e90938275
devbyrieza <devbyrieza@gmail.com>
2026-07-07 12:52:35 +0700

    chore: remove test account filter in admin view for demo purposes

--- src/lib/utils/admin.ts
+++ src/lib/utils/admin.ts
@@ -8,20 +8,6 @@ export function getAdminWhereClause(tahunAjaranId?: string): any {
   const where: any = {
     deleted_at: null,
     status_pendaftaran: { not: "mengundurkan_diri" },
-    NOT: [
-      {
-        AND: [
-          { nama_lengkap: { contains: " Tes", mode: "insensitive" } },
-          {
-            NOT: {
-              nama_lengkap: { contains: "Rieza Tes", mode: "insensitive" },
-            },
-          },
-        ],
-      },
-      { nama_lengkap: { startsWith: "TEST ", mode: "insensitive" } },
-      { nama_lengkap: { contains: "BYPASS", mode: "insensitive" } },
-    ],
   };
 
   if (tahunAjaranId) {
